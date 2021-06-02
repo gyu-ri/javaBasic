7277aeee123142bcad4a213cbc8b6f27e17cac0d
HGR <HGR@DESKTOP-3DGEN61>
2021-06-02 19:36:36 +0900

    잔디 에러 해결

--- JavaBasicStudy/src/chapter4/SwitchCase.java
+++ JavaBasicStudy/src/chapter4/SwitchCase.java
@@ -5,7 +5,7 @@ public class SwitchCase {
 	public static void main(String[] args) {
 		int rank = 1;
 		char medalColor;
-		
+		//스위치문
 		switch(rank) {
 			case 1:medalColor = 'G';
 				  // break;    //이거 안쓰면 break 만날때까지 수행을 하기 때문에 break를 써야 한다
