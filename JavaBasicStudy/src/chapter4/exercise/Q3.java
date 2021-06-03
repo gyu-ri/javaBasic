161ff3a417c4cb17e25d7a2bc49df2ab067b87ba
gyu-ri <ghkdrbfl93@gmail.com>
2021-06-04 01:10:52 +0900

    test

--- JavaBasicStudy/src/chapter4/exercise/Q3.java
+++ JavaBasicStudy/src/chapter4/exercise/Q3.java
@@ -3,7 +3,7 @@ package chapter4.exercise;
 public class Q3 {
 
 	public static void main(String[] args) {
-		//구구단을 단보다 곱하는 수가 작거나 같은 경우까지만 출력하는 프로그램을 만들어 보세요 (break문 사용)
+		//구구단을 단보다 곱하는 수가 작거나 같은 경우까지만 출력하는 프로그램을 만들어 보세요 (break문을 이용해 보자)
 
 	}
 
