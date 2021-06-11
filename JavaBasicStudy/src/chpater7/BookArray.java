dc6c2d88c68fc931b4d6a9e580a795d9b0b8e1e4
gyu-ri <ghkdrbfl93@gmail.com>
2021-06-11 21:52:16 +0900

    array

--- JavaBasicStudy/src/chpater7/BookArray.java
+++ JavaBasicStudy/src/chpater7/BookArray.java
@@ -10,11 +10,11 @@ public class BookArray {
 			System.out.println(library[i]);          //배열 안에 객체의 내용을 안넣어 줘서 null 뜸
 		}
 		
-		library[0] = new Book("태백산맥1", "조정래");
-		library[1] = new Book("태백산맥2", "조정래");
-		library[2] = new Book("태백산맥3", "조정래");
-		library[3] = new Book("태백산맥4", "조정래");
-		library[4] = new Book("태백산맥5", "조정래");
+		library[0] = new Book("가나다", "홍길동");
+		library[1] = new Book("라마바", "김유신");
+		library[2] = new Book("사아자", "강감찬");
+		library[3] = new Book("차카타파하", "이성계");
+		library[4] = new Book("훈민정음", "세종대왕");
 		
 		for(int i = 0; i<library.length; i++) {   
 			System.out.println(library[i]);          //객체의 내용을 넣어 줘서 주소값이 뜸
